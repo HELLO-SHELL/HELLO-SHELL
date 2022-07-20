3e032471465ed4c0b59714c4dbc967f4e67862c2
Jaehwan Kim <jaekim@c4r10s5.42seoul.kr>
2022-07-20 15:25:23 +0900

    HS-30 FIX: solve memory leak when show env's len

--- src/main.c
+++ src/main.c
@@ -9,17 +9,9 @@ int main(int ac, char **av, char **env)
     int key_length = 0;
     int value_length = 0;
 
-    // while (env[i])
-    // {
-    //     printf("Before List : %s\n", env[i]);
-    //     i++;
-    // }
-    // printf("---------------------------------");
-    // key 와 value 는 길이를 측정하면서 malloc해야함
-    // env[i] 를 = 을 기준으로 앞뒤를 나누고, env_contents 의 key와 value에 길이로 malloc하고 할당
-    while (*env)
+    i = 0;
+    while (env[i])
     {
-        i = 0;
         j = 0;
         env_list = (t_list *)malloc(sizeof(t_list));
         if (env_list == 0)
@@ -28,12 +20,6 @@ int main(int ac, char **av, char **env)
         env_list->content = (t_env *)malloc(sizeof(t_env));
         if (env_list->content == 0)
             return (0);
-
-        env_contents = (t_env *)malloc(sizeof(t_env));
-        if (env_contents == 0)
-            return (0);
-
-        env_list->content = env_contents;
         
         while (env[i][j] != '=')
         {
@@ -51,7 +37,9 @@ int main(int ac, char **av, char **env)
         printf("\n------------------------------------------\n");
         key_length = 0;
         value_length = 0;
-        env++;
+        free (env_list->content);
+        free (env_list);
+        i++;
     }
     /*----------------------------------------------*/
     
