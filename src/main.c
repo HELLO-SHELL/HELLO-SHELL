9fc21e2632a534d1356519f54da6a857c1f0d9dc
Jaehwan Kim <jaekim@c4r10s5.42seoul.kr>
2022-07-20 15:13:22 +0900

    HS-30 FEAT: display env's key and value len

--- src/main.c
+++ src/main.c
@@ -1,54 +1,58 @@
 #include "../include/minishell.h"
 
-static t_env	*ft_env_lstlast(t_env *lst)
-{
-	if (!lst)
-		return (NULL);
-	while (lst->next)
-		lst = lst->next;
-	return (lst);
-}
-
-static void	ft_env_lstadd_back(t_env **lst, t_env *new)
-{
-	t_env	*result;
-
-	result = ft_env_lstlast(*lst);
-	if (!result)
-		*lst = new;
-	else
-		result->next = new;
-}
-
-static t_env	*ft_env_new(void *value)
-{
-	t_env	*result;
-
-	result = (t_env *)malloc(sizeof(t_env));
-	if (!result)
-		return (NULL);
-	result->value = value;
-	result->next = NULL;
-	return (result);
-}
-
 int main(int ac, char **av, char **env)
 {
-    // char    *input;
-    t_env   *env_list;
-    t_env   *curr;
-    int i = 0;
+    t_list  *env_list;
+    t_env   *env_contents;
+    int i;
+    int j;
+    int key_length = 0;
+    int value_length = 0;
 
-    // printf("%s\n", env[0]);
-    while (env[i])
+    // while (env[i])
+    // {
+    //     printf("Before List : %s\n", env[i]);
+    //     i++;
+    // }
+    // printf("---------------------------------");
+    // key 와 value 는 길이를 측정하면서 malloc해야함
+    // env[i] 를 = 을 기준으로 앞뒤를 나누고, env_contents 의 key와 value에 길이로 malloc하고 할당
+    while (*env)
     {
-        printf("Before List : %s", env[i]);
-        i++;
+        i = 0;
+        j = 0;
+        env_list = (t_list *)malloc(sizeof(t_list));
+        if (env_list == 0)
+            return (0);
+
+        env_list->content = (t_env *)malloc(sizeof(t_env));
+        if (env_list->content == 0)
+            return (0);
+
+        env_contents = (t_env *)malloc(sizeof(t_env));
+        if (env_contents == 0)
+            return (0);
+
+        env_list->content = env_contents;
+        
+        while (env[i][j] != '=')
+        {
+            key_length++;
+            j++;
+        }
+        j++;
+        while (env[i][j] != '\0')
+        {
+            value_length++;
+            j++;
+        }
+        printf("%s\n", env[i]);
+        printf("key : %d, value : %d\n", key_length, value_length);
+        printf("\n------------------------------------------\n");
+        key_length = 0;
+        value_length = 0;
+        env++;
     }
-    printf("---------------------------------");
-    // 여기부터 다시 진행해야함.
-    // env 구조체의 char *value 도 malloc을 해야함.
-    // malloc을 하려면, strlen로 각 문자열의 길이를 구해야 함.
     /*----------------------------------------------*/
     
     // while(1)
