790696e2846c37d11de0709a0123c152ab6af5c1
Jaehwan Kim <jaekim@c4r10s5.42seoul.kr>
2022-07-20 15:45:50 +0900

    HS-30 FEAT: Done split key & value

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@ int main(int ac, char **av, char **env)
 {
     t_list  *env_list;
     t_env   *env_contents;
+    char    *key;
+    char    *value;
     int i;
     int j;
     int key_length = 0;
@@ -12,6 +14,7 @@ int main(int ac, char **av, char **env)
     i = 0;
     while (env[i])
     {
+        // 초기화
         j = 0;
         env_list = (t_list *)malloc(sizeof(t_list));
         if (env_list == 0)
@@ -20,7 +23,7 @@ int main(int ac, char **av, char **env)
         env_list->content = (t_env *)malloc(sizeof(t_env));
         if (env_list->content == 0)
             return (0);
-        
+        // key & value 길이 구하는 코드
         while (env[i][j] != '=')
         {
             key_length++;
@@ -32,13 +35,40 @@ int main(int ac, char **av, char **env)
             value_length++;
             j++;
         }
+        // key & value 초기화
+        key = (char *)malloc(key_length + 1);
+        if (key == 0)
+            return (0);
+        value = (char *)malloc(value_length + 1);
+        if (key == 0)
+            return (0);
+        // 구한 길이로 key & value 만드는 곳
+        int x = 0;
+        int y = 0;
+        while (x < key_length)
+        {
+            key[x] = env[i][x];
+            x++;
+        }
+        key[x] = '\0';
+        x++;
+        while (y < value_length)
+        {
+            value[y] = env[i][x];
+            x++;
+            y++;
+        }
+        value[y] = '\0';
         printf("%s\n", env[i]);
-        printf("key : %d, value : %d\n", key_length, value_length);
-        printf("\n------------------------------------------\n");
+        printf("lens key: %d, value: %d\n", key_length, value_length);
+        printf("data key: %s, value: %s\n", key, value);
+        printf("\n-------------------------------------\n");
         key_length = 0;
         value_length = 0;
         free (env_list->content);
         free (env_list);
+        free (key);
+        free (value);
         i++;
     }
     /*----------------------------------------------*/
