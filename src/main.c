041098d7b05425d4d2d17a2bbeea2b9998e6f5c7
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-07-21 21:16:59 +0900

    HS-32 CHORE: undo comments

--- src/main.c
+++ src/main.c
@@ -5,16 +5,16 @@ int main(int ac, char **av, char **env)
     char *input;
 
     print_wallpaper(); 
-    // while(1)
-    // {
-    //     input = readline("HELLO-SHELL-0.0$ ");
-    //     if (input)
-    //         printf("%s\n", input);
-    //     else
-    //         break ;
-    //     add_history(input);
-    //     free(input);
-    // }
-    // system("leaks minishell");
+    while(1)
+    {
+        input = readline("HELLO-SHELL-0.0$ ");
+        if (input)
+            printf("%s\n", input);
+        else
+            break ;
+        add_history(input);
+        free(input);
+    }
+    system("leaks minishell");
     return(0);
 }
