8ee293764268f7fe78ef2fd884ac274d5f1bf3f9
김재환 <woghks7859@gmail.com>
2022-07-28 01:28:27 +0900

    HS-42 FEAT: check invalid input and add_lst func

--- src/built_in/ft_export.c
+++ src/built_in/ft_export.c
@@ -1,20 +1,69 @@
 #include "../../include/minishell.h"
 
+int	export_error_check(t_token *token)
+{
+	t_token	*curr;
+
+	curr = token;
+	while (curr->next != NULL)
+	{
+		if (curr->next->value[0] == '='
+			|| ft_isdigit(curr->next->value[0]) || curr->next->value[0] == '-')
+			return (1);
+		curr = curr->next;
+	}
+	if (curr->value[0] == '='
+			|| ft_isdigit(curr->value[0]) || curr->value[0] == '-')
+			return (1);
+	return (0);
+}
+// export: usage: export [-nf] [name[=value] ...] or export -p
 void	ft_export(t_node *minishell)
 {
 	t_list	*curr;
 	t_token *token;
+	t_env	*env_node;
+	char *key;
+	char *value;
 
 	curr = minishell->env_list;
 	token = minishell->token_list;
-
-	// 두번째 value 체크
 	if (token->next == NULL)
 	{
 		while (curr->next)
 		{
-			printf("declare -x %s=%s\n", ((t_env *)(curr->content))->key, ((t_env *)(curr->content))->value);	
-			curr = curr->next;
+			// value 에 값이 없으면 다음으로 넘어감
+			if (!strchr(((t_env *)(curr->content))->value, 0))
+				curr = curr->next;
+			else
+			{
+				printf("declare -x %s=%s\n", ((t_env *)(curr->content))->key, ((t_env *)(curr->content))->value);	
+				curr = curr->next;
+			}
+		}
+		printf("declare -x %s=%s\n", ((t_env *)(curr->content))->key, ((t_env *)(curr->content))->value);
+	}
+	else
+	{
+		while (token->next)
+		{
+			if (export_error_check(token))
+			{
+                write(2,"HELLO-SHELL: `", 14);
+                write(2, token->next->value, ft_strlen(token->next->value));
+                write(2, "': command not found\n", 22);
+			}
+			else
+			{
+				env_node = malloc(sizeof(t_env));
+				key = ft_substr(token->next->value, 0, ft_strchr(token->next->value, '=') - token->next->value);
+				value = ft_substr(ft_strchr(token->next->value, '=') + 1, 0, ft_strlen(ft_strchr(token->next->value, '=')));
+				env_node->key = key;
+				env_node->value = value;
+				ft_lstadd_back(&(minishell->env_list), ft_lstnew(env_node));// = 확인, 앞 뒤로 쪼개서 키&값 넣기
+				// 덮어씌우기 구현
+			}
+			token = token->next;
 		}
 	}
 }
