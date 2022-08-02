2b4a7e35aa762be85963ad0b9cca29068387eaf6
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-08-02 14:00:19 +0900

    HS-38 FEAT: exit와 에러관련 함수

--- src/built_in/ft_exit.c
+++ src/built_in/ft_exit.c
@@ -1,13 +1,8 @@
 #include "../../include/minishell.h"
 
-int	ft_exit(int status)
+void	ft_exit(int status, char *err_msg) // 추후 minishell 구조체가 확정되면 수정 필요
 {
-	char *err_msg;
-
-	err_msg = "error message";
-	write(2, RED, ft_strlen(RED));
-	write(2, err_msg, ft_strlen(err_msg));
-	write(2, COLOR_RESET, ft_strlen(COLOR_RESET));
+	if (*err_msg)
+		print_error_message(err_msg);
 	exit(status);
-	return (0);
 }
