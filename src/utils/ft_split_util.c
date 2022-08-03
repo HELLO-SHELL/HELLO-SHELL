61976c07e9b2f972a67a8cab248e1f77552b0040
jaekim <85930183+jae-hwan-kim@users.noreply.github.com>
2022-08-03 22:10:17 +0900

    Hs 49 따옴표 제거 토큰 파서로 수정하기 (#18)
    * HS-49 FEAT: change return value in func
    
    * HS-49 WIP: add quotes func
    
    * HS-49 WIP: start quotes in export func
    
    * HS-49 WIP: doing extract quotes
    
    * HS-49 WIP: quotes func, but not debug & fill str
    
    * HS-49 WIP: fill str but has bug("ab"cd'ef'&"a bc")
    
    * HS-49 WIP: find where the bugs generate
    
    * HS-49 FEAT: check quotes even&odd
    
    * HS-49 FEAT: parse white space in quotes
    
    Co-authored-by: Jaehwan Kim <jaekim@c2r7s4.42seoul.kr>

--- src/utils/ft_split_util.c
+++ src/utils/ft_split_util.c
@@ -1,5 +1,52 @@
 #include "../../include/minishell.h"
 
+int	handle_quote(char *line, int *count, int *idx)
+{
+	int	quotes;
+	int	i;
+	int	single_quotes;
+	int	double_quotes;
+
+	// 홀수 체크 check_odd_quotes(line)
+	i = 0;
+	single_quotes = 0;
+	double_quotes = 0;
+	while (line[i])
+	{
+		if (line[i] == '\'')
+			single_quotes++;
+		else if (line[i] == '\"')
+			double_quotes++;
+		i++;
+	}
+	if ((single_quotes % 2) || (double_quotes % 2))
+		return (0);
+	while (line[*idx] != '\0')
+	{
+		// "abc"de
+		if (line[*idx] == '\'' || line[*idx] == '\"')
+		{
+			quotes = line[*idx];
+			(*idx)++;
+		}
+		while ((line[*idx] != '\'' || line[*idx] != '\"')
+				&& (ft_isalnum(line[*idx]) || (line[*idx] >= 9 && line[*idx] <= 13)
+				|| line[*idx] == 32) ) // 따옴표 확인
+		{
+			(*idx)++;
+			(*count)++;
+			if ((line[*idx] == '\'' || line[*idx] == '\"'))
+				quotes = line[*idx];
+		}
+		if ((quotes != line[*idx]) && (line[*idx] != '\0'))
+			return (1);
+		else if (line[*idx] == '\0')
+			return (0);
+		(*idx)++;
+	}
+	return (0);
+}
+
 int	check_white_space(char c)
 {
 	if ((c >= 9 && c <= 13) || c == 32)
@@ -42,12 +89,35 @@ int	split_line(char *line, char **str, int *i)
 	}
 	while (!check_white_space(line[*i]) && line[*i] != '\0')
 	{
+		if (line[*i] == 34 || line[*i] == 39)
+		{
+			if (handle_quote(line, &rtn, i))
+				return (0);
+			break ;
+		}
 		rtn++;
 		(*i)++;
 	}
 	(*str) = malloc(sizeof(char) * rtn + 1);
 	if (!(*str))
 		exit(EXIT_FAILURE);
-	ft_strlcpy((*str), line + (*i) - rtn, rtn + 1);
+	// line, 0부터 쿼트 뺴고 넣기
+	int	z;
+	int	j;
+
+	z = 0;
+	j = 0;
+	while (z < rtn)
+	{
+		if (line[j] == '\'' || line[j] == '\"')
+			j++;
+		else
+		{
+			(*str)[z] = line[j];
+			z++;
+			j++;
+		}
+	}
+	(*str)[z] = '\0';
 	return (rtn);
 }
