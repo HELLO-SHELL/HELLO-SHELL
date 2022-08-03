9e5d0a692471775e70121e6968bde89ec862427e
김재환 <woghks7859@gmail.com>
2022-08-03 22:11:10 +0900

    HOTFIX: print env

--- src/built_in/ft_env.c
+++ src/built_in/ft_env.c
@@ -3,7 +3,7 @@
 void	ft_env(t_list *env)
 {
 	// = 있는지 없는지 확인하기
-	while (env && env->next != NULL)
+	while (env)
 	{
 		if (((t_env *)(env->content))->value != NULL)
 			printf("%s=%s\n", ((t_env *)(env->content))->key, ((t_env *)(env->content))->value);
