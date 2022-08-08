9e0fcb5a89a935b3c34d4d84ba87d7808a976e08
yeonseong <yeonseong.dev@gmail.com>
2022-08-08 13:25:29 +0900

    HS-58 환경변수 key값으로 찾기 (#23)
    * HS-58 FEAT: 환경변수를 key값으로 찾는 함수
    
    * HS-58 TEST: get_env_by_key 유닛테스트

---
+++ src/utils/get_env_by_key.c
@@ -0,0 +1,15 @@
+#include "../../include/minishell.h"
+
+t_env *get_env_by_key(t_list *env_list, char *key)
+{
+    t_list  *temp;
+
+    temp = env_list;
+    while (temp)
+	{
+		if (is_same_string(((t_env *)(temp->content))->key, key))
+			return ((t_env *)temp->content);
+		temp = temp->next;
+	}
+    return (NULL);
+}
