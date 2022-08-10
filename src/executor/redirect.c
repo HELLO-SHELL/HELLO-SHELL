ff1aa804faede49fbf8b2fa3cb42beb16b9d7430
jhMin95 <tesla52@naver.com>
2022-08-10 14:25:41 +0900

    HS-57 FEAT: redirect 관련 의사코드 작성

---
+++ src/executor/redirect.c
@@ -0,0 +1,11 @@
+#include "../../include/minishell.h"
+
+void	apply_redirection(t_token *cmd_line)
+{
+	// 돌면서 redirect 를 찾습니다. < > >>
+	// TK_RDINPUT TK_RDOUTPUT TK_APPEND
+	
+	// 다음 파일을 open(filename);
+	// input 이면 dup2(파일, stdin_fileno) ...
+	// 닫아줘야 할까요? close(filename)
+}
