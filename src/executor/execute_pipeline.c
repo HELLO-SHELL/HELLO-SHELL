3c0c444c608884286ca2bda8a6c35d1806fb0e18
jhMin95 <tesla52@naver.com>
2022-08-10 15:34:46 +0900

    HS-57 WIP: accessable.c 의사코드 생성, accessable 관련 의사코드 분리

--- src/executor/execute_pipeline.c
+++ src/executor/execute_pipeline.c
@@ -1,6 +1,5 @@
 #include "../../include/minishell.h"
 
-int	is_accessable();
 void	redirection(t_process *ps_list, int ps_id)
 {
 	//ps_list 의 ps_id 번째 node 의 head
@@ -15,21 +14,22 @@ void	execute_pipeline(t_process *ps_list)
 
 	//while (fork 뜨기(총 process 개수)
 	//	ps_id
-	//	redirection(ps_list, ps_id);
-	//	access command 검사 (실행가능한지, built-in 함수인지)
+	//	apply_redirection(ps_list, ps_id);
+	//	is_accessable() command 검사 (실행가능한지, built-in 함수인지)
 	//	if built-in 함수면
 	//		execute_builtin
 	//	else if 실행가능하면
 	//		execute_command
 	//	else
 	//		에러 출력
-
 }
 	
 void	heredoc_to_temp(t_process *ps_list)
 {
-	//while 문으로 ps_list -> next로 넘기기
-	
+	//	fork() 떠서 실행. 이유는 ctrl-C 를 처리하기 위해.
+	//	ctrl-C 입력이 들어오면 비정상 종료 시그널 넘기고 파이프라인 실행 종료
+	//	다음 shell 입력받을 준비 (prompt 출력)
+	//
 	//	while ps_list->head 를	
 	//		type이 herecoc 이면 next 를 delim으로 받고
 	//		temp 파일을 write로 open CREAT 옵션으로 생성하고 open
@@ -40,8 +40,7 @@ void	heredoc_to_temp(t_process *ps_list)
 	//		close
 }
 
-
-int	executor(t_process *ps_list)
+void	executor(t_process *ps_list)
 {
 	// ps_list 에 있는 node를 순회하면서
 	
