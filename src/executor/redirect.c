6769fa4cc9e23c148c37a0cb4f67633dca86de49
jhMin95 <tesla52@naver.com>
2022-08-12 00:05:44 +0900

    HS-57 FEAT: redirection 코드 작성

--- src/executor/redirect.c
+++ src/executor/redirect.c
@@ -14,15 +14,26 @@ void	apply_redirection(char *filename, int mode)
 		dup2(fd, STDOUT_FILENO);
 	}
 	else if (mode == TK_APPEND)
+	{
 		fd = openfile(filename, APPEND);
+		dup2(fd, STDOUT_FILENO);
+	}
+	// 닫아줘야 할까요? close(filename)
 }
 
 void	apply_redirections(t_token *cmd_line)
 {
-	// 돌면서 redirect 를 찾습니다. < > >>
-	// TK_RDINPUT TK_RDOUTPUT TK_APPEND
-	
-	// 다음 파일을 open(filename);
-	// input 이면 dup2(파일, stdin_fileno) ...
-	// 닫아줘야 할까요? close(filename)
+	t_token	*curr;
+
+	curr = cmd_line;
+	while (curr != NULL)
+	{
+		if (curr->type == TK_RDINPUT)
+			apply_redirection(curr->next->value, TK_RDINPUT);
+		else if (curr->type == TK_RDOUTPUT)
+			apply_redirection(curr->next->value, TK_RDOUTPUT);
+		else if (curr->type == TK_APPEND)
+			apply_redirection(curr->next->value, TK_APPEND);
+		curr = curr->next;
+	}
 }
