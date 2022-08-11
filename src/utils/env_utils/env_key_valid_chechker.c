b1eead595943c949c0f71fce3ba3cdbc8989e994
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-08-11 20:47:13 +0900

    HS-59 FIX:  add '_'

--- src/utils/env_utils/env_key_valid_chechker.c
+++ src/utils/env_utils/env_key_valid_chechker.c
@@ -5,14 +5,16 @@ int	env_key_valid_chechker(char *str)
 	char *temp;
 
 	temp = str;
+	if (*temp = '?' && *(temp + 1) == NULL)
+		return (ENV_KEY_QUESTION);
 	if (!(ft_isalpha(*temp) || *temp != '_'))
-		return (0);
+		return (FALSE);
 	temp++;
 	while (*temp)
 	{
-		if (!ft_isalnum(*temp))
-			return (0);
+		if (!ft_isalnum(*temp) || *temp != '_')
+			return (FALSE);
 		temp++;
 	}
-	return (1);
+	return (ENV_KEY_OTHERS);
 }
