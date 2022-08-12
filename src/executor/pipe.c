6d10585e183a308d30d06a24a967eb76768de532
Ji-Hyuck, Min <45284810+jhMin95@users.noreply.github.com>
2022-08-12 16:33:43 +0900

    HS-67-pipe-util-함수들-추가하기 (#36)
    HS-67 FEAT: 추가 완료, 구조체 추가, 헤더 약간 변경

--- src/executor/pipe.c
+++ src/executor/pipe.c
@@ -1,1 +1,42 @@
-//	jimin의 pipex
+#include "../../include/minishell.h"
+
+static void	_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	swap_pipe(t_pipes *p)
+{
+	_swap(&(p->prev_pipe[READ]), &(p->next_pipe[READ]));
+	_swap(&(p->prev_pipe[WRITE]), &(p->next_pipe[WRITE]));
+}
+
+void	init_pipe(t_pipes *p)
+{
+	p->prev_pipe[READ] = -1;
+	p->prev_pipe[WRITE] = -1;
+	p->next_pipe[READ] = -1;
+	p->next_pipe[WRITE] = -1;
+}
+
+void	close_pipe(int *fd)
+{
+	if (*fd != -1)
+	{
+		if (close(*fd))
+			print_error_message("fail close()\n");
+		*fd = -1;
+	}
+}
+
+void	close_pipes(t_pipes *p)
+{
+	close_pipe(&p->prev_pipe[READ]);
+	close_pipe(&p->prev_pipe[WRITE]);
+	close_pipe(&p->next_pipe[READ]);
+	close_pipe(&p->next_pipe[WRITE]);
+}
