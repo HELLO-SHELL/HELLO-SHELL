a4898bd126c0f05c5a43fd8538b04ca4528b8b02
김재환 <woghks7859@gmail.com>
2022-08-13 10:05:29 +0900

    HS-65 CHORE: test했던 sh 파일 원상 복구

--- test/unit_tester.sh
+++ test/unit_tester.sh
@@ -1,5 +1,4 @@
 #!/bin/bash/sh
 
-cc -lreadline ./built_in_unit_test.c ../src/built_in/ft_pwd.c -o unit_test
+cc -lreadline ./built_in_unit_test.c ../src/built_in/*.c ../src/utils/*.c ../lib/*/*.c -o unit_test
 ./unit_test;
-#  ../src/utils/*.c ../lib/*/*.c
