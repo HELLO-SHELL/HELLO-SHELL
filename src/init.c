4d94c70fdd3e38dc546b8a725b13372d465acf3d
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-08-13 20:52:29 +0900

    HOTFIX: 오타수정

--- src/init.c
+++ src/init.c
@@ -17,17 +17,18 @@ void    init_minishell(t_minishell *minishell)
 		input = readline("HELLO-SHELL-0.0$ DEBUG ");
 		if (!input)
 			exit(EXIT_SUCCESS);
+		add_history(input);
 		input_buffer = replace_whole_input_dollar(input, minishell);
 		str = command_split(input_buffer);
+		free(input_buffer);
 		minishell->ps_list.cmd_line = set_token_list(str);
 		curr = minishell->ps_list.cmd_line;
-		add_history(input);
+		set_process_list(&ps_list, minishell->ps_list.cmd_line);
 		if (input_buffer)
 			executor(minishell);
 		else
 			break ;
 		// system("leaks minishell");
-		free(input_buffer);
 		i = -1;
 		while (str[++i])
 			free(str[i]);
@@ -36,14 +37,9 @@ void    init_minishell(t_minishell *minishell)
 		t_token *temp;
 		while (curr)
 		{
-		t_token	*temp;
-		while (curr)
-		{
 			temp = curr;
 			free(temp);
 			curr = curr->next;
 		}
-		// set_process_list(&ps_list, minishell->ps_list.cmd_line);
-		system("leaks minishell");
 	}
 }
