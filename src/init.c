431fb6eaf76f03075ed724b9d6d31a0ae6b97cc5
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-08-13 21:02:31 +0900

    HOTFIX: execute wait must be fixed   Don't Panic!!!   현재 코드에서 실행이 이상하게 되는게 정상입니다. 놀라지마세요. wait가   추가되지 않아 이상한것처럼 보이는 것입니다.

--- src/init.c
+++ src/init.c
@@ -14,7 +14,7 @@ void    init_minishell(t_minishell *minishell)
 		signal(SIGINT, get_new_prompt);
 		signal(SIGQUIT, SIG_IGN);
 		ps_list = NULL;
-		input = readline("HELLO-SHELL-0.0$ DEBUG ");
+		input = readline("HELLO-SHELL-0.0$ :");
 		if (!input)
 			exit(EXIT_SUCCESS);
 		add_history(input);
@@ -22,13 +22,13 @@ void    init_minishell(t_minishell *minishell)
 		str = command_split(input_buffer);
 		free(input_buffer);
 		minishell->ps_list.cmd_line = set_token_list(str);
-		curr = minishell->ps_list.cmd_line;
 		set_process_list(&ps_list, minishell->ps_list.cmd_line);
+		curr = minishell->ps_list.cmd_line;
 		if (input_buffer)
-			executor(minishell);
+			// executor(minishell); wait 추가되야  함.
+			printf("%s \n", input_buffer); // exexutor 수정 후 executor로 대체 예정
 		else
 			break ;
-		// system("leaks minishell");
 		i = -1;
 		while (str[++i])
 			free(str[i]);
