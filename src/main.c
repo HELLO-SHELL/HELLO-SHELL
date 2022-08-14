539ebee5ca2f6656c92de44b9bddd6646e48da93
yeonseong <yeonseong@yeonseongs-MacBook-Pro.local>
2022-08-14 23:43:41 +0900

    HOTFIX: minishell must be allocated in heap.

--- src/main.c
+++ src/main.c
@@ -2,10 +2,11 @@
 
 int main(int ac, char **av, char **env)
 {
-	t_minishell	minishell;
+	t_minishell	*minishell;
 
+	minishell = safe_malloc(sizeof(t_minishell));
 	print_wallpaper(); 
-	env_linked_list(&minishell, env);
-	init_minishell(&minishell);
+	env_linked_list(minishell, env);
+	init_minishell(minishell);
 	return (0);
 }
