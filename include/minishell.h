f39ead10a127aafd4bd6e9109dd1caeb498af50d
yeonseong <yeonseong.dev@gmail.com>
2022-08-15 13:57:48 +0900

    HS-72 FEAT: $가 여러개 있을 경우에도 대응 (#41)
    * HS-72 FEAT: value 를 반환하는 함수 추가
    
    * HS-72 FEAT: value 를 반환하는 함수 추가
    
    * HS-72 FIX: error
    
    * HS-72 FEAT: $ multi replacement
    
    * HS-72 FEAT: 달러와 달러사이도 append 해줌
    
    Co-authored-by: = <=>

--- include/minishell.h
+++ include/minishell.h
@@ -93,6 +93,7 @@ void	print_error_message(char *str);
 void	ft_error_exit(char *str);
 void	get_new_prompt(int sig);
 t_env	*get_env_by_key(t_list *env_list, char *key);
+char	*get_env_value_by_key(t_list *env_list, char *key);
 int		env_key_valid_checker(char *str);
 void	*ft_memccpy_under(void *dest, const void *src, int c, size_t n);
 char	*replace_whole_input_dollar(char *input, t_minishell *minishell);
