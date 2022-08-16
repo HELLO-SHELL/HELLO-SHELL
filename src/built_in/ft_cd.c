597d287e994359c965ad0bfdef1d67e77a2bde2c
Jaehwan Kim <jaekim@c2r7s8.42seoul.kr>
2022-08-16 17:27:17 +0900

    HS-71 WIP: refactoring ft_cd

--- src/built_in/ft_cd.c
+++ src/built_in/ft_cd.c
@@ -1,29 +1,59 @@
 #include "../../include/minishell.h"
 
+int go_to_home_directory(t_list *env_list, t_token *token_list, char *path)
+{
+    if (token_list == NULL || token_list->value[0] == '~'
+        || (token_list->value[0] == '~' && token_list->value[1] == '/'))
+    {
+        path = get_env_by_key(env_list, "HOME")->value;
+        return (1);
+    }
+    return (0);
+}
+
+int go_to_root_directory(t_list *env_list, t_token *token_list, char *path)
+{
+    char *root;
+
+    root = "/";
+    if (token_list->value[0] == '/')
+    {
+        path = root;
+        return (1);
+    }
+    return (0);
+}
+
+void    go_to_path_directory(t_token **token_list, char *path)
+{
+    path = (*token_list)->value;
+}
+
 void  ft_cd(t_node *minishell)
 {
     t_list *env_list;
     t_token *token_list;
     char *path;
-    char *root;
-    
-    root = "/";
+
     path = NULL;
     env_list = minishell->env_list;
     token_list = minishell->token_list->next;
     // go_to_home_directory
-    if (token_list == NULL || token_list->value[0] == '~'
-        || (token_list->value[0] == '~' && token_list->value[1] == '/'))
-        path = get_env_by_key(env_list, "HOME")->value;
-    else if (token_list->value[0] == '/')
-        path = root;
+    // go_to_root
+    // go_to_path
+    // update_env_pwd
+    if (go_to_home_directory(env_list, token_list, path))
+        ;
+    else if (go_to_root_directory(env_list, token_list, path))
+        ;
     else
-        path = token_list->value;
+        go_to_path_directory(&token_list, path);
+
     // 예외 조건에 대한 에러 출력 필요
-    // OLDPWD 수정
     if (chdir(path) != 0)
             print_error_message("fail CD _ 수정 필요");
     // PWD 수정
+    // undate_env_pwd();
     t_env *change_pwd;
 
     change_pwd = get_env_by_key(env_list, "PWD");
@@ -31,3 +61,5 @@ void  ft_cd(t_node *minishell)
     change_pwd->value = NULL;
     change_pwd->value = getcwd(NULL, 0);
 }
+
+// OLDPWD 수정
