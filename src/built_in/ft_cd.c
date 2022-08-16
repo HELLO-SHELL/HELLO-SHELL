e982ab376890944c59263b74e5b04c33b8e869da
김재환 <woghks7859@gmail.com>
2022-08-16 20:47:31 +0900

    HS-71 FEAT: solve ~/path exception

--- src/built_in/ft_cd.c
+++ src/built_in/ft_cd.c
@@ -2,10 +2,26 @@
 
 int go_to_home_directory(t_list *env_list, t_token *token_list, char **path)
 {
-    if (token_list == NULL || token_list->value[0] == '~'
-        || (token_list->value[0] == '~' && token_list->value[1] == '/'))
+    if (token_list == NULL
+        || (token_list->value[0] == '~' && token_list->value[1] == '\0')
+        || (token_list->value[0] == '~' && token_list->value[1] == '/'
+            && token_list->value[2] == '\0'))
     {
         *path = get_env_by_key(env_list, "HOME")->value;
+        return (1);
+    }
+    return (0);
+}
+
+int go_to_path_directory_via_home(t_list *env_list, t_token *token_list, char **path)
+{
+    if (token_list->value[0] == '~' && token_list->value[1] == '/')
+    {
+        *path = get_env_by_key(env_list, "HOME")->value;
+        if (chdir(*path) != 0)
+            print_error_message("bash: cd: No such file or directory");
+        *path = NULL;
+        *path = ft_strchr(token_list->value, '/') + 1;
         return (1);
     }
     return (0);
@@ -32,9 +48,12 @@ void  ft_cd(t_node *minishell)
     char *path;
 
     path = NULL;
+    change_pwd = NULL;
     env_list = minishell->env_list;
     token_list = minishell->token_list->next;
     if (go_to_home_directory(env_list, token_list, &path))
+        ;
+    else if (go_to_path_directory_via_home(env_list, token_list, &path))
         ;
     else
         go_to_path_directory(token_list, &path);
