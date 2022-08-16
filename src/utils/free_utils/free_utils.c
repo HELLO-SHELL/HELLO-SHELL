cd5c46a5cb53eea68db9e5b7710509b670c37555
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-08-16 16:57:02 +0900

    HS-77 FIX: all free, multi process

--- src/utils/free_utils/free_utils.c
+++ src/utils/free_utils/free_utils.c
@@ -25,16 +25,16 @@ void    free_all(t_minishell *minishell, char *replaced_input, char **splitted_i
 	while (splitted_input[++i])
 		safe_free(splitted_input[i]);
 	safe_free(splitted_input);
-	curr_token = minishell->ps_list->cmd_line;
-	while (curr_token)
-	{
-		temp_token = curr_token;
-		curr_token = curr_token->next;
-		safe_free(temp_token);
-	}
 	curr_process = minishell->ps_list;
-	while (curr_process)
+	while (curr_process && curr_process->cmd_line)
 	{
+		curr_token = curr_process->cmd_line;
+		while (curr_token)
+		{
+			temp_token = curr_token;
+			curr_token = curr_token->next;
+			safe_free(temp_token);
+		}
 		temp_process = curr_process;
 		curr_process = curr_process->next;
 		safe_free(temp_process);
