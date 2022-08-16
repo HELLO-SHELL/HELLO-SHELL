d3adc12b26396a88e6d3a2b5784d2d1fc3748af0
Jaehwan Kim <jaekim@c2r7s8.42seoul.kr>
2022-08-16 17:16:14 +0900

    HS-71 FEAT: Done cd & update PWD

--- src/built_in/ft_cd.c
+++ src/built_in/ft_cd.c
@@ -1,47 +1,33 @@
 #include "../../include/minishell.h"
-/*
-1. 메시지를 전달하 듯한 변수명 사용해볼까?
-2. 사용 함수 chdir, getcwd..?
-3. 에러 처리?
-*/
+
 void  ft_cd(t_node *minishell)
 {
     t_list *env_list;
     t_token *token_list;
     char *path;
-
+    char *root;
+    
+    root = "/";
     path = NULL;
     env_list = minishell->env_list;
     token_list = minishell->token_list->next;
     // go_to_home_directory
-    if (token_list == NULL || token_list->value[0] == '~')
+    if (token_list == NULL || token_list->value[0] == '~'
+        || (token_list->value[0] == '~' && token_list->value[1] == '/'))
         path = get_env_by_key(env_list, "HOME")->value;
+    else if (token_list->value[0] == '/')
+        path = root;
+    else
+        path = token_list->value;
+    // 예외 조건에 대한 에러 출력 필요
     // OLDPWD 수정
     if (chdir(path) != 0)
-        print_error_message("fail CD _ 수정 필요");
+            print_error_message("fail CD _ 수정 필요");
     // PWD 수정
     t_env *change_pwd;
 
     change_pwd = get_env_by_key(env_list, "PWD");
     free(change_pwd->value);
     change_pwd->value = NULL;
-    change_pwd->value = ft_strdup(path);
-    
-    /*
-    if (cd 뒤에 토큰 존재 여부)
-        없으면 홈 디렉토리로 이동
-    else if (토큰 있으면 에러 확인 isalpnum)
-        있으면 에러 출력
-    else if (~, / 있는지 확인)
-        if (~있으면)
-            홈 디렉토리
-        else if (/있으면)
-            루트 디렉토리
-    else
-        그 다음 문자열 확인
-        if(현재 디렉토리에 있는 파일 이름이 있는지 확인)
-            있으면 이동
-        else
-            없으면 에러 출력
-    */
+    change_pwd->value = getcwd(NULL, 0);
 }
