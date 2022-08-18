52de656c5b1f2f2f968d5c460ee8beff0d8d17e7
Jaehwan Kim <jaekim@c2r6s2.42seoul.kr>
2022-08-18 16:43:39 +0900

    HS-84 FEAT: is_built_in

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -2,17 +2,27 @@
 
 int	is_built_in(t_process *ps_info)
 {
-	int	condition;
+	char *cmd;
 
-	condition = 1; // must be rewrited, and delete comments!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	if (condition == 1)
+	cmd = ps_info->cmd_line->value[0];
+	if (cmd == CD
+		|| cmd == ENV
+		|| cmd == PWD
+		|| cmd == EXIT
+		|| cmd == ECHO
+		|| cmd == UNSET
+		|| cmd == EXPORT
+		)
 		return (1);
-	else
-		return (0);
+	return (0);
 };
 
 void	execute_built_in(t_process *process)
 {
+	char *cmd;
+
+	cmd = process->cmd_line->value[0];
+	
 	return ;
 };
 
@@ -33,6 +43,7 @@ void	execute_process(t_process *ps_info, t_pipes *pipes)
 	safe_dup2(pipes->prev_pipe[READ], STDIN_FILENO);
 	safe_dup2(pipes->next_pipe[WRITE], STDOUT_FILENO);
 	safe_close_pipes(pipes);
+	// 여기
 	if (is_built_in(ps_info))
 		execute_built_in(ps_info);
 	else
@@ -71,6 +82,7 @@ void	execute_single_cmdline(t_process *process)
 {
 	pid_t	pid;
 	apply_redirections(process->cmd_line);
+	// 여기
 	if (is_built_in(process))
 		execute_built_in(process);
 	else
