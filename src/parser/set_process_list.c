d047b4f007b6aeaf05ab55b1c33d873d0e02f9b5
keinn51 <soogood97@gmail.com>
2022-08-22 17:47:08 +0900

    HS-85 FEAT: process 구조체에 size 추가하기

--- src/parser/set_process_list.c
+++ src/parser/set_process_list.c
@@ -67,5 +67,6 @@ void set_process_list(t_process	**ps_list, t_token *tk_list)
 
 	pipe_cnt = count_pipe(tk_list);
 	init_pslist(ps_list, pipe_cnt);
+	(*ps_list)->size = pipe_cnt + 1;
 	insert_process_head(ps_list, tk_list);
 }
