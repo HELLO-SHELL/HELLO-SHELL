e22a6a10d9e1e3633f0fbbad0c90bb5786423f7e
jaekim <85930183+jae-hwan-kim@users.noreply.github.com>
2022-08-23 12:47:36 +0900

    Update unit_tester.sh

--- test/unit_tester.sh
+++ test/unit_tester.sh
@@ -1,5 +1,4 @@
 #!/bin/bash/sh
 
-# cc -lreadline -L /Users/jaekim/.brew/opt/readline/lib -I /Users/jaekim/.brew/opt/readline/include ./built_in_unit_test.c ../src/init.c ../src/built_in/*.c ../src/utils/*.c ../src/executor/*.c ../src/parser/*.c ../src/welcome/*.c ../lib/*/*.c -o unit_test
-cc -lreadline -L /Users/jaekim/.brew/opt/readline/lib -I /Users/jaekim/.brew/opt/readline/include ./built_in_unit_test.c ../src/built_in/ft_echo.c ../src/utils/*.c ../src/parser/split/*.c ../src/utils/chore_utils/safe_malloc.c ../lib/*/*.c -g3 -o unit_test
+cc -lreadline -L ${HOME}/.brew/opt/readline/lib -I ${HOME}/.brew/opt/readline/include ./built_in_unit_test.c ../src/built_in/ft_echo.c ../src/utils/*.c ../src/parser/split/*.c ../src/utils/chore_utils/safe_malloc.c ../lib/*/*.c -g3 -o unit_test
 ./unit_test;
