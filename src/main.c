93c0386ce16d925e7794e90854edd3c1cf96851e
Jaehwan Kim <jaekim@c2r2s3.42seoul.kr>
2022-08-25 10:05:23 +0900

    HS-96 FEAT: Done main.c & wallpaper except 주석, header

--- src/main.c
+++ src/main.c
@@ -2,10 +2,10 @@
 
 t_minishell	g_minishell_info;
 
-int main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **env)
 {
 	print_wallpaper();
-	init_minishell_info();	
+	init_minishell_info();
 	env_linked_list(env);
 	init_minishell();
 	return (0);
