b68a92a93c6a1a81d488f001ed289e1bc4e706f9
keinn51 <soogood97@gmail.com>
2022-08-26 22:23:07 +0900

    HS-109 FEAT: special token 에 대한 예외처리

---
+++ src/parser/token_count.c
@@ -0,0 +1,32 @@
+#include "../../include/minishell.h"
+
+int	token_word_count(t_token *token)
+{
+	t_token	*token_curr;
+	int		count;
+
+	count = 0;
+	token_curr = token;
+	while (token_curr)
+	{
+		if (token_curr->type == TK_WORD)
+			count++;
+		token_curr = token_curr->next;
+	}
+	return (count);
+}
+
+int	all_token_count(t_token *token)
+{
+	t_token	*token_curr;
+	int		count;
+
+	count = 0;
+	token_curr = token;
+	while (token_curr)
+	{
+		count++;
+		token_curr = token_curr->next;
+	}
+	return (count);
+}
