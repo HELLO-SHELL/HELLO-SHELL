2e450193568e219944baa3eb92b270eea494afb2
Jaehwan Kim <jaekim@c2r4s4.42seoul.kr>
2022-08-26 16:18:15 +0900

    HS-96 FEAT: 줄&헤더&주석 제외하고 놈 함

--- src/built_in/ft_cd.c
+++ src/built_in/ft_cd.c
@@ -1,42 +1,42 @@
 #include "../../include/minishell.h"
 
-static void    update_env_pwd(void)
+static void	update_env_pwd(void)
 {
-    t_env *change_pwd;
+	t_env	*change_pwd;
 
-    change_pwd = get_env_by_key("PWD");
-    free((change_pwd)->value);
-    (change_pwd)->value = NULL;
-    (change_pwd)->value = getcwd(NULL, 0);
+	change_pwd = get_env_by_key("PWD");
+	free(change_pwd->value);
+	change_pwd->value = NULL;
+	change_pwd->value = getcwd(NULL, 0);
 }
 
-static void    set_path(char **path)
+static void	set_path(char **path)
 {
-    t_list *env_list;
-    t_token *cmd_list;
+	t_list	*env_list;
+	t_token	*cmd_list;
 
-    env_list = g_minishell_info.env_list;
-    cmd_list = g_minishell_info.ps_list->cmd_line->next;
-    if (is_path_only_home(cmd_list))
-        set_path_to_home(env_list, cmd_list, path);
-    else if (is_path_pass_home(cmd_list))
-        set_path_at_home(env_list, cmd_list, path);
-    else
-        set_path_to_input(cmd_list, path);
+	env_list = g_minishell_info.env_list;
+	cmd_list = g_minishell_info.ps_list->cmd_line->next;
+	if (is_path_only_home(cmd_list))
+		set_path_to_home(env_list, cmd_list, path);
+	else if (is_path_pass_home(cmd_list))
+		set_path_at_home(env_list, cmd_list, path);
+	else
+		set_path_to_input(cmd_list, path);
 }
 
-static void change_directory(char **path)
+static void	change_directory(char **path)
 {
-    if (chdir(*path) != 0)
-        print_error_message("hello-shell: cd: No such file or directory");
+	if (chdir(*path) != 0)
+		print_error_message("hello-shell: cd: No such file or directory");
 }
 
-void  ft_cd(void)
+void	ft_cd(void)
 {
-    char *path;
+	char	*path;
 
-    path = NULL;
-    set_path(&path);
-    change_directory(&path);
-    update_env_pwd();
+	path = NULL;
+	set_path(&path);
+	change_directory(&path);
+	update_env_pwd();
 }
--- src/built_in/ft_unset.c
+++ src/built_in/ft_unset.c
@@ -1,56 +1,56 @@
 #include "../../include/minishell.h"
 
-static void    unsetting(t_list *env_list, t_token *token)
+static void	unsetting(t_list *env_list, t_token *token)
 {
-    t_list  *curr;
-    t_list  *temp;
-    t_list  *env_prev;
+	t_list	*curr;
+	t_list	*temp;
+	t_list	*env_prev;
 
-    curr = env_list;
-    while (curr)
-    {
-        temp = curr;
-        if (is_same_string(((t_env *)temp->content)->key, token->value))
-        {
-            if (token->value == ((t_env *)temp->content)->key)
-                env_list = temp->next;
-            else
-                env_prev->next = temp->next;
-            safe_free (((t_env *)temp->content)->key);
-            safe_free (((t_env *)temp->content)->value);
-            safe_free((temp->content));
-        }
-        else
-            env_prev = temp;
-        curr = curr->next;
-    }
+	curr = env_list;
+	while (curr)
+	{
+		temp = curr;
+		if (is_same_string(((t_env *)temp->content)->key, token->value))
+		{
+			if (token->value == ((t_env *)temp->content)->key)
+				env_list = temp->next;
+			else
+				env_prev->next = temp->next;
+			safe_free (((t_env *)temp->content)->key);
+			safe_free (((t_env *)temp->content)->value);
+			safe_free((temp->content));
+		}
+		else
+			env_prev = temp;
+		curr = curr->next;
+	}
 }
 
-static int unset_have_no_parm(t_token *token)
+static int	unset_have_no_parm(t_token *token)
 {
-    if (token->next == NULL)
-    {
-        token = token->next;
-        return (1);
-    }
-    token = token->next;
-    return (0);
+	if (token->next == NULL)
+	{
+		token = token->next;
+		return (1);
+	}
+	token = token->next;
+	return (0);
 }
 
-void    ft_unset(void)
+void	ft_unset(void)
 {
-    t_list  *env_list;
-    t_token *token;
+	t_list	*env_list;
+	t_token	*token;
 
-    env_list = g_minishell_info.env_list;
-    token = g_minishell_info.ps_list->cmd_line;
-    if (unset_have_no_parm(token))
-        return ;
-    token = token->next;
-    while (token)
-    {
-        unsetting(env_list, token);
-        token = token->next;
-    }
-    return ;
+	env_list = g_minishell_info.env_list;
+	token = g_minishell_info.ps_list->cmd_line;
+	if (unset_have_no_parm(token))
+		return ;
+	token = token->next;
+	while (token)
+	{
+		unsetting(env_list, token);
+		token = token->next;
+	}
+	return ;
 }
