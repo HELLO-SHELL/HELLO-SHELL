cf532d1d38cf3c8f6611fcd3a3bc45ecc9ef8878
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-08-26 21:24:10 +0900

    HS-105 FEAT: add replace_dollar_in_heredoc function

--- src/utils/replace_dollar/replace_dollar.c
+++ src/utils/replace_dollar/replace_dollar.c
@@ -82,3 +82,19 @@ char	*replace_whole_input_dollar(char *input)
 	input = NULL;
 	return (input_buffer);
 }
+
+char	*replace_dollar_in_heredoc(char *input)
+{
+	char	*input_buffer;
+	char	*input_ptr;
+
+	if (!ft_strchr(input, '$'))
+		return (input);
+	input_buffer = safe_malloc(ft_strlen(input));
+	input_ptr = input;
+	input_buffer = append_buffer_under_dollar(input_buffer, input_ptr);
+	make_dollar_replaced_input(&input_buffer, &input_ptr);
+	free(input);
+	input = NULL;
+	return (input_buffer);
+}
