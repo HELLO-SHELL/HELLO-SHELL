2828c7fb487d61c752af238046d32193f959f871
keinn51 <soogood97@gmail.com>
2022-08-27 15:31:29 +0900

    HS-111 REFACT: check token 함수분리

--- src/parser/check_token_error.c
+++ src/parser/check_token_error.c
@@ -1,27 +1,38 @@
 #include "../../include/minishell.h"
 
+static char	*check_near_token(t_token *token)
+{
+	char	*err_msg;
+
+	if (token->prev && is_special_token(token->prev->type))
+		err_msg = "special token can't stuck each other";
+	else if (token->next && is_special_token(token->next->type))
+		err_msg = "special token can't stuck each other";
+	else if (token->prev == NULL && token->next == NULL)
+		err_msg = "special token can't exist alone";
+	return (err_msg);
+}
+
 int	check_token_error(t_token *token)
 {
 	t_token	*token_curr;
-	int		is_error;
+	char	*err_msg;
 
-	is_error = FALSE;
 	token_curr = token;
 	while (token_curr)
 	{
 		if (token_curr->prev == NULL && is_same_string(token_curr->value, "|"))
-			is_error = print_error_with_new_prompt("pipe can not be located in the first location.");
-		else if (token_curr->next == NULL && is_same_string(token_curr->value, "|"))
-			is_error = print_error_with_new_prompt("pipe can not be located in the last location.");
+			err_msg = "pipe can not be located in the first location.";
+		else if (token_curr->next == NULL
+			&& is_same_string(token_curr->value, "|"))
+			err_msg = "pipe can not be located in the last location.";
 		else if (is_special_token(token_curr->type))
+			err_msg = check_near_token(token_curr);
+		if (!is_same_string("", err_msg))
 		{
-			if (token_curr->prev && is_special_token(token_curr->prev->type))
-				is_error = print_error_with_new_prompt("special token can't stuck each other");
-			if (token_curr->next && is_special_token(token_curr->next->type))
-				is_error = print_error_with_new_prompt("special token can't stuck each other");
-		}
-		if (is_error)
+			print_error_with_new_prompt(err_msg);
 			return (TRUE);
+		}
 		token_curr = token_curr->next;
 	}
 	return (FALSE);
