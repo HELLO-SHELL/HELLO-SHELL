9a19c0d94fcafe7d73792110fbffb7ffedb6a456
Jaehwan Kim <jaekim@c5r6s1.42seoul.kr>
2022-08-29 18:45:27 +0900

    HS-132 FEAT: finish adding header & norm

--- src/utils/signal.c
+++ src/utils/signal.c
@@ -6,22 +6,24 @@
 /*   By: jaekim <jaekim@student.42seuol.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 17:09:35 by jaekim            #+#    #+#             */
-/*   Updated: 2022/08/29 17:09:36 by jaekim           ###   ########.fr       */
+/*   Updated: 2022/08/29 18:42:30 by jaekim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	get_new_prompt(void)
+void	get_new_prompt(int input)
 {
+	(void) input;
 	rl_replace_line("", 0);
 	write(1, "\n", 1);
 	rl_on_new_line();
 	rl_redisplay();
 }
 
-void	heredoc_new_prompt(void)
+void	heredoc_new_prompt(int input)
 {
+	(void) input;
 	rl_replace_line("", 0);
 	write(1, "\n", 1);
 	rl_on_new_line();
--- test/utils_fun_unit_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash/sh
-
-cc -lreadline ./utils_fun_unit_test.c ../src/*/*.c ../lib/*/*.c -lreadline -o unit_test
-./unit_test;
