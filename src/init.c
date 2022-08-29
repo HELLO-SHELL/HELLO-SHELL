a864de8031dcd440eb82cdc0733b69c6e664e894
jimin <tesla52@naver.com>
2022-08-29 18:35:44 +0900

    HS-132 REFACT: init.c norminette

--- src/init.c
+++ src/init.c
@@ -6,17 +6,34 @@
 /*   By: jaekim <jaekim@student.42seuol.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 16:59:19 by jaekim            #+#    #+#             */
-/*   Updated: 2022/08/29 16:59:20 by jaekim           ###   ########.fr       */
+/*   Updated: 2022/08/29 18:31:58 by jimin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+void	execute_minishell(char *input)
+{
+	t_token		*curr_token;
+	char		*replaced_input;
+
+	replaced_input = replace_whole_input_dollar(input);
+	if (replaced_input && *replaced_input)
+	{
+		curr_token = make_token_list(command_split(replaced_input));
+		if (check_token_error(curr_token))
+			return ;
+		set_process_list(&g_minishell_info.ps_list, curr_token);
+		make_node_to_envp();
+		executor();
+		free_all();
+	}
+}
+
 void	init_minishell(void)
 {
 	t_token		*curr_token;
 	char		*input;
-	char		*replaced_input;
 
 	while (1)
 	{
@@ -27,19 +44,7 @@ void	init_minishell(void)
 			exit(ft_atoi(g_minishell_info.last_status));
 		add_history(input);
 		if (quote_validator(input))
-		{
-			replaced_input = replace_whole_input_dollar(input);
-			if (replaced_input && *replaced_input)
-			{
-				curr_token = make_token_list(command_split(replaced_input));
-				if (check_token_error(curr_token))
-					continue ;
-				set_process_list(&g_minishell_info.ps_list, curr_token);
-				make_node_to_envp();
-				executor();
-				free_all();
-			}
-		}
+			execute_minishell(input);
 		else
 			free(input);
 	}
